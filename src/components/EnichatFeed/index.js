4c2d68b0653cbbe8863b078272a572f2c86a3143
Enigmage <alizaidi0786.az@gmail.com>
2021-05-18 01:25:55 +0530

    no chat div

--- src/components/EnichatFeed/index.js
+++ src/components/EnichatFeed/index.js
@@ -2,19 +2,27 @@ import MessageForm from "./MessageForm";
 import MessageBubbles from "./MessageBubbles";
 
 const EnichatFeed = (chatAppProps) => {
-    const { chats, activeChat, userName, messages, userSecret} = chatAppProps;
+    const { chats, activeChat, userName, messages, userSecret } = chatAppProps;
     const chat = chats && chats[activeChat];
-    if (!chat) return <div />;
     return (
         <div className="chat-feed">
-            <div className="chat-title-container">
-                <div className="chat-title">#{chat.title}</div>
-            </div>
-            <MessageBubbles userName={userName} messages={messages} chatId={activeChat} userSecret={userSecret} />
-            <div style={{ height: "100px" }} />
-            <div className="message-form-container">
-                <MessageForm {...chatAppProps} chatId={activeChat} />
-            </div>
+            {chat && (
+                <div>
+                    <div className="chat-title-container">
+                        <div className="chat-title">#{chat.title}</div>
+                    </div>
+                    <MessageBubbles
+                        userName={userName}
+                        messages={messages}
+                        chatId={activeChat}
+                        userSecret={userSecret}
+                    />
+                    <div style={{ height: "100px" }} />
+                    <div className="message-form-container">
+                        <MessageForm {...chatAppProps} chatId={activeChat} />
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
