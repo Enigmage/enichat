97e2c5d8c165a7488ac4baa44d18d516aa7adaf4
Enigmage <alizaidi0786.az@gmail.com>
2021-05-18 02:13:25 +0530

    unused import

--- src/components/Forms/SignupForm.js
+++ src/components/Forms/SignupForm.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link, Redirect } from "react-router-dom";
 import { rememberUser, useForm } from "../../utils";
 
